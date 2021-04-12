f8ae0034eac43af39a8fe9325bae8cbdcf24e237
Christian Neil Anthony Wico <cnawico@gmail.com>
2021-04-12 11:47:09 +0800

    Create Constants file.

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const AUTH0_DOMAIN = 'cwic-personal.us.auth0.com';
+  static const AUTH0_CLIENT_ID = 'YUhdHz8arNQWs06XXxHTqk355oH5MMAr';
+  static const AUTH0_REDIRECT_URI =
+      'com.example.fluttercodechallenge://login-callback';
+  static const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
+  static const SECURE_REFRESH_TOKEN = 'secure_refresh_token';
+}
